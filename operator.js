b9bcf8f2bf432bc58a841298ba696aa063abf6bc
Ukhasyah Ganteng <ukasyaaah@gmail.com>
2025-07-25 23:09:25 +0700

    operator

---
+++ operator.js
@@ -0,0 +1,36 @@
+// Aritmatika
+console.log(2 + 2);
+console.log(2 - 2);
+console.log(2 * 2);
+console.log(2 / 2);
+
+// Sisa Pembagian
+console.log(2 % 2);
+
+var x = 3;
+var y = 2;
+
+// x = x * 2
+console.log((x *= 2));
+
+x % 2 == 0;
+if (x && y == 2) console.log("yey");
+
+// operator string (+)
+a = "apple";
+b = "pen";
+
+c = 20;
+d = "25";
+console.log(a + " " + b);
+console.log(c + d);
+
+// Operator typeof -> cek tipe data
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+
+// ternary operator
+cek = c % 2 == 0;
+console.log(cek);
+console.log(cek ? "miaw" : "false!");
