046da49084508037191cfc277a3119871e7d8265
Ukhasyah Ganteng <ukasyaaah@gmail.com>
2025-07-26 00:03:28 +0700

    variable

---
+++ variable.js
@@ -0,0 +1,21 @@
+// Deklarasi variable
+/**
+ * Gaboleh pake spasi, pake _
+ * boleh angka tp ! diawali angka
+ * boleh pake dollar $, (diawal / diakhir bebas)
+ *
+ * sering dipake: camelCase
+ */
+
+// deklarasi & inisialisasi
+var x;
+
+// assignment
+x = 20;
+
+// Short hand
+var nama = "Ari",
+  jurusan = "RPL",
+  lulus = true;
+
+// Variable Scope
