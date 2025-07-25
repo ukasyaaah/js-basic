9e01412402bfc89359405b8cc5427fe34c131cff
Ukhasyah Ganteng <ukasyaaah@gmail.com>
2025-07-25 23:28:12 +0700

    bool & string

---
+++ boolean.js
@@ -0,0 +1,10 @@
+x = 12;
+
+// false
+console.log(x == 13);
+
+// truw
+console.log(x == 12);
+
+// true
+console.log(Boolean(x < 13));
---
+++ string.js
@@ -0,0 +1,26 @@
+// klo ada kutip 1 dalam teks, gunakan kutip 2
+"Hari Jum'at";
+
+// Escape character
+console.log("Gerakan hari jum'at");
+
+// new line
+console.log("Gerakan hari \njumat");
+
+// tab
+console.log("Gerakan hari \tjumat");
+
+// backspace
+console.log("Gerakan hari \bjumat");
+
+console.log("\u00a9"); // copyright
+
+/// Konkatenasi
+
+cek = "Ukhasyah Fauzan" == "ukhasyah fauzan";
+console.log(cek);
+
+// .length -> hitung panjang string
+// spasi juga dihitung
+leng = "ini panjang".length;
+console.log(leng);
