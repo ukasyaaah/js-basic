4016f7440919d58813cfdf846607bd0e8610e364
Ukhasyah Ganteng <ukasyaaah@gmail.com>
2025-07-26 20:10:47 +0700

    for

---
+++ latihan/angkot2.js
@@ -0,0 +1,12 @@
+jmlAngkot = 10;
+angkotBeroperasi = 6;
+noAngkot = 1;
+
+while (noAngkot <= angkotBeroperasi) {
+  console.log(`Angkot No. ${noAngkot} beroperasi dengan baik.`);
+  noAngkot++;
+}
+
+for (noAngkot = angkotBeroperasi + 1; noAngkot <= jmlAngkot; noAngkot++) {
+  console.log(`Angkot No. ${noAngkot} sedang tidak beroperasi.`);
+}
