6911c5211507a9ae9492e62b0807a02de696ae4c
Ukhasyah Ganteng <ukasyaaah@gmail.com>
2025-07-26 19:57:02 +0700

    while

---
+++ angkot1.js
@@ -0,0 +1,6 @@
+jmlAngkot = 10;
+no = 1;
+while (no <= jmlAngkot) {
+  console.log(`Angkot No. ${no} beroperasi dengan baik.`);
+  no++;
+}
---
+++ pengulangan/while.js
@@ -0,0 +1,5 @@
+ulang = 0;
+while (ulang <= 5) {
+  console.log(`hehhlo world ${ulang}x`);
+  ulang++;
+}
