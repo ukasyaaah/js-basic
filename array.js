f20effef5c8ebff7704505f1d698c4d61047fd5a
Ukhasyah Ganteng <ukasyaaah@gmail.com>
2025-07-27 14:19:08 +0700

    array method

---
+++ array.js
@@ -0,0 +1,20 @@
+array = ["a", 1, true, [4, 5, 6]];
+
+console.log(array[3][1]);
+
+alamat = ["Semarang", "Central Java"];
+console.log(alamat.join(" - "));
+
+// Push : nambah elemen diakhir array
+alamat.push("Indonesia", "Bumi");
+
+//Pop : hapus elemen diakhir array
+alamat.pop();
+console.log(alamat);
+
+// Shif : Hapus elemen diawal array
+alamat.shift()
+
+// Unshift : tambah elemen diawal array
+alamat.unshift('Masaran', 'Sragen')
+console.log(alamat);
