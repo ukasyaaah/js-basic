dceaa367fc96de7760f8447b2d8944c632aa87a1
Ukhasyah Ganteng <ukasyaaah@gmail.com>
2025-07-27 20:18:43 +0700

    Hapus penumpang

--- angkot7.js
+++ angkot7.js
@@ -1,4 +1,4 @@
-var penumpang = ["sanghika", undefined, "doddy"];
+var penumpang = ["asnawi", "amba", "akwikwok"];
 
 tambahPenumpang = function (nama, penumpang) {
   for (i = 0; i < penumpang.length; i++) {
@@ -20,6 +20,31 @@ tambahPenumpang = function (nama, penumpang) {
   }
 };
 
+hapusPenumpang = function (nama, penumpang) {
+  for (i = 0; i <= penumpang.length; i++) {
+    if (penumpang.length === 0) {
+      console.log("Angkotnya kosong");
+      return penumpang;
+    } else {
+      if (penumpang[i] == nama) {
+        penumpang[i] = undefined;
+        return penumpang;
+      } else if (penumpang.find((cari) => cari == nama) == null) {
+        console.log(`${nama} ga ada di angkot`);
+        return penumpang;
+      }
+    }
+  }
+};
+
 console.log(tambahPenumpang("hadil", penumpang));
 console.log(tambahPenumpang("hadil", penumpang));
 console.log(tambahPenumpang("asnawi", penumpang));
+console.log(hapusPenumpang("miaw", penumpang));
+console.log(hapusPenumpang("asnawi", penumpang));
+console.log(hapusPenumpang("amba", penumpang));
+console.log(tambahPenumpang("billie", penumpang));
+console.log(tambahPenumpang("brian", penumpang));
+console.log(hapusPenumpang("heh", penumpang));
+console.log(hapusPenumpang("akwikwok", penumpang));
+
